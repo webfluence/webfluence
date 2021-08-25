3265cb203a5a4537c642c3f21b56d604be9f5186
Doug Henderson <dhend595@gmail.com>
2021-08-25 10:11:24 -0400

    extra pages created

--- client/components/Donate.js
+++ client/components/Donate.js
@@ -3,3 +3,13 @@ import { connect } from "react-redux";
 import SearchBar from "./SearchBar";
 import Footer from "./Footer";
 import { makeStyles } from "@material-ui/core/styles";
+
+export const Donate = (props) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <SearchBar />
+      <Footer />
+    </div>
+  );
+}
--- client/components/Sources.js
+++ client/components/Sources.js
@@ -3,3 +3,14 @@ import { connect } from "react-redux";
 import SearchBar from "./SearchBar";
 import Footer from "./Footer";
 import { makeStyles } from "@material-ui/core/styles";
+
+
+export const Sources = (props) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <SearchBar />
+      <Footer />
+    </div>
+  );
+}
