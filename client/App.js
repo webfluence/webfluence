c607545ff8a649140431eaf84da42d6f81ee8315
Sakib Hossain <sakiba09@gmail.com>
2021-08-30 13:05:38 -0400

    added toast notifications

--- client/App.js
+++ client/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
 
 const App = () => {
   return (
@@ -8,7 +8,7 @@ const App = () => {
       <Navbar />
       <Routes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
