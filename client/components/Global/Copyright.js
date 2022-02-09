415b0989033e1fe2c86e9558071a480042dedc31
Doug Henderson <dhend595@gmail.com>
2022-02-08 19:28:50 -0500

    test

--- client/components/Global/Copyright.js
+++ client/components/Global/Copyright.js
@@ -1,15 +1,15 @@
-import React from "react"
+// import React from "react"
 
 
-export default Copyright = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        {"https://webfluence.herokuapp.com "}
-      </Link>
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+// export default Copyright = () => {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {"Copyright © "}
+//       <Link color="inherit" href="https://material-ui.com/">
+//         {"https://webfluence.herokuapp.com "}
+//       </Link>
+//       {new Date().getFullYear()}
+//       {"."}
+//     </Typography>
+//   );
+// }
